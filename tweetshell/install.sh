9f4fef9c3a539443000393287d43052e3579c5d5
sirwanmd <108808654+sirwanmd@users.noreply.github.com>
2022-07-16 14:05:16 +0300

    Update install.sh

--- tweetshell/install.sh
+++ tweetshell/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#Author: github.com/thelinuxchoice
-#Instagram: @thelinuxchoice
+#Author: github.com/sirwanmd
+#Instagram: @sirwanmd
 trap 'echo exiting cleanly...; exit 1;' SIGINT SIGTSTP
 
 checkroot() {
